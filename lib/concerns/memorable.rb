c867fb133d2d582d73ef7dfb3db1f277d011d941
wei chen <weichen9083@gmail.com>
2020-02-08 20:47:30 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
-
-  def reset_all
-    self.all.clear
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
   end
-
-  def count
-    self.all.count
+ 
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
   end
-  
-end 
+end
